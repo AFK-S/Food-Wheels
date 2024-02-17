b94f3ded8c34df03f33f955755035ab644664eb3
FaizanPotrick <faizan.potrick02@gmail.com>
2024-02-17 16:57:10 +0530

    Connect dish form

--- food-truck-client/src/pages/NewDish.js
+++ food-truck-client/src/pages/NewDish.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { TextInput, Checkbox, Textarea, Button, Group, Box, Select } from "@mantine/core";
+import {
+  TextInput,
+  Checkbox,
+  Textarea,
+  Button,
+  Group,
+  Select,
+} from "@mantine/core";
 import { useForm } from "@mantine/form";
+import axios from "axios";
 
 const NewDish = () => {
-  const category = [
-    "Beverages",
-    "Breakfast",
-    "Lunch",
-    "Dinner",
-    "Dessert",
-    "Snacks",
-  ];
+  const category = ["starter", "main course", "dessert"];
 
   const food_type = ["veg", "non-veg", "jain"];
 
@@ -21,26 +22,40 @@ const NewDish = () => {
       description: "",
       category: "",
       food_type: "",
-      is_signature: false,
       price: 0,
       discount: 0,
-      coverImage: null,
+      is_signature: false,
     },
   });
 
-
-  useEffect(() => {
-    form.setFieldValue("coverImage", image);
-  }, [image]);
-
   const finalPrice = Math.max(0, form.values.price - form.values.discount);
+
+  const handleSubmit = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("name", form.values.name);
+      formData.append("description", form.values.description);
+      formData.append("category", form.values.category);
+      formData.append("food_type", form.values.food_type);
+      formData.append("price", form.values.price);
+      formData.append("discount", finalPrice);
+      formData.append("coverImage", image);
+      formData.append("is_signature", form.values.is_signature);
+      const { data } = await axios.post("/api/dish", formData);
+      console.log(data);
+      form.reset();
+      setImage(null);
+    } catch (err) {
+      alert(err.response?.data?.message || err.message || err);
+    }
+  };
 
   return (
     <>
       <div className="c-card p-4 container-fluid">
-        <h3>Add New Item</h3>
+        <h3>Add New Dish</h3>
 
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+        <form onSubmit={form.onSubmit(handleSubmit)}>
           <div className="row mt-2 gy-3">
             <div className="col-md-6">
               <TextInput
@@ -65,6 +80,7 @@ const NewDish = () => {
                 withAsterisk
                 label="Dish Description"
                 placeholder="Describe the dish"
+                {...form.getInputProps("description")}
               />
             </div>
             <div className="col-md-6">
@@ -96,9 +112,16 @@ const NewDish = () => {
             </div>
 
             <div className="col-md-6 d-flex">
-              <p className="fw-medium me-3">Select Dish Image <span style={{
-                color: "red"
-              }}>*</span></p>
+              <p className="fw-medium me-3">
+                Select Dish Image{" "}
+                <span
+                  style={{
+                    color: "red",
+                  }}
+                >
+                  *
+                </span>
+              </p>
               <div>
                 <input
                   type="file"
@@ -106,27 +129,37 @@ const NewDish = () => {
                     setImage(e.target.files[0]);
                   }}
                 />
-                {image ?
-                  <div style={{
-                    width: "150px",
-                    height: "150px",
-                    overflow: "hidden",
-                    borderRadius: "10px",
-                    marginTop: "10px"
-                  }}>
-                    <img src={URL.createObjectURL(image)} alt="dish" className="img-fluid" />
+                {image ? (
+                  <div
+                    style={{
+                      width: "150px",
+                      height: "150px",
+                      overflow: "hidden",
+                      borderRadius: "10px",
+                      marginTop: "10px",
+                    }}
+                  >
+                    <img
+                      src={URL.createObjectURL(image)}
+                      alt="dish"
+                      className="img-fluid"
+                    />
                   </div>
-
-                  : null}
+                ) : null}
               </div>
-
             </div>
 
             <div className="col-md-6">
               {form.values.price !== 0 && (
                 <h3>Final Price: Rs. {finalPrice}</h3>
               )}
+              <Checkbox
+                mt="md"
+                label="Is Signature Dish"
+                {...form.getInputProps("is_signature", { type: "checkbox" })}
+              />
             </div>
+
             <Group justify="flex-end">
               <Button type="submit" className="mt-3">
                 Submit
--- server/routes/dish.routes.js
+++ server/routes/dish.routes.js
@@ -15,37 +15,7 @@ const fieldHandler = require("../middlewares/fieldHandler.middleware");
 
 const router = express.Router();
 
-router.post(
-  "/",
-
-  body("name").trim().notEmpty().withMessage("Dish name is required"),
-  body("description").trim().notEmpty().withMessage("Description is required"),
-  body("category").trim().notEmpty().withMessage("Category is required"),
-  body("food_type").trim().notEmpty().withMessage("Food type is required"),
-  body("price")
-    .trim()
-    .notEmpty()
-    .withMessage("Price is required")
-    .isNumeric()
-    .withMessage("Price should be numeric"),
-  body("discount")
-    .trim()
-    .notEmpty()
-    .withMessage("Discount is required")
-    .isNumeric()
-    .withMessage("Discount should be numeric"),
-  body("is_signature")
-    .trim()
-    .notEmpty()
-    .withMessage("Signature dish is required")
-    .isBoolean()
-    .withMessage("Signature dish should be boolean"),
-
-  fieldHandler,
-  upload.single("coverImage"),
-  uploadAvatar,
-  create
-);
+router.post("/", upload.single("coverImage"), uploadAvatar, create);
 
 router.get("/", findAll);
 
