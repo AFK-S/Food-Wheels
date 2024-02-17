603b77be4ad63321cbf28d510a17e78ebc5cc7db
Karan2310 <karandeep9619@gmail.com>
2024-02-17 20:03:43 +0530

    Top Bar Sticky

--- food-truck-client/src/components/TopBar/TopBar.js
+++ food-truck-client/src/components/TopBar/TopBar.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./TopBar.css";
 
 const TopBar = ({ ToggleMenu }) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 110) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <div className="topbar container-fluid py-3  py-md-2 mb-3 p-2 px-md-3 ">
+      <div
+        className={`topbar container-fluid py-3  py-md-2 mb-3 p-2 px-md-3 ${isScrolled ? "active" : ""
+          }`}
+      >
         <div className="row w-100 flexbox">
           <div className="col-9 col-md-10 d-flex align-items-center ">
             <i
