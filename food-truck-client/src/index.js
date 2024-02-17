48808e41c2b41e9252326e0126bca87dc94dcd96
FaizanPotrick <faizan.potrick02@gmail.com>
2024-02-17 16:20:31 +0530

    Remove redux

--- food-truck-client/src/index.js
+++ food-truck-client/src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { CookiesProvider } from "react-cookie";
-import { Provider } from "react-redux";
-import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <CookiesProvider>
-        <App />
-      </CookiesProvider>
-    </Provider>
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
   </React.StrictMode>
 );
